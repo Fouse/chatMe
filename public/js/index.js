df77c6a00fccc00a53d255651f7937f2d3623ee8
Fousseini Konate <Fous@Fousseinis-MacBook-Air.local>
2018-06-18 01:24:37 -0400

    added form and show incoming message on the screen

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,35 @@
 var socket = io();
 socket.on('connect', function() {
     console.log('connected to server.');
-    // socket.emit('createEmail', {
-    //     to: 'fous@gmail.com',
-    //     text: 'Hey!'
-    // });
-    // socket.emit('createMessage', {
-    //     From: 'fk@example.com',
-    //     Text: 'some text.'
-    // })
 });
+
 socket.on('diconnect', function() {
     console.log('disconnected from server');
 });
-// socket.on('newEmail', function(email) {
-//     console.log('new email', email);
-// });
+
 socket.on('newMessage', function(message) {
     console.log('new message', message);
+    var newMsg = $(`<li>${message.from}: ${message.text} ${message.createdAt}</li>`);
+
+    $('#messages').append(newMsg);
+});
+
+// socket.emit('createMessage', {
+//     from: 'Jeff',
+//     text: 'hello'
+// }, function(data) {
+//     console.log('Got it', data);
+
+// });
+$(document).ready(function() {
+    console.log("ready!");
+});
+$('#message-form').on('submit', function(e) {
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, function() {
+
+    });
 });
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-
 const path = require('path');
+
+const { generateMessage } = require('./utils/message');
+// const message = require('./utils/message');
+// const generateMessage = message.generateMessage;
 
 const publicPath = path.join(__dirname, '../public');
 // console.log(publicPath);
@@ -18,22 +21,12 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected!');
 
-    // socket.emit('newMessage', {
-    //     From: 'James',
-    //     Text: 'some text',
-    //     createdAt: 1234
-    // });
-
-    // socket.on('createEmail', (newEmail) => {
-    //     console.log('createEmail', newEmail);
-    // });
-    socket.on('createMessage', (message) => {
+    socket.emit('newMessage', generateMessage('Admin', 'welcome to the chat app'));
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+    socket.on('createMessage', (message, callback) => {
         console.log('message created', message);
-        io.emit('newMessage', {
-            From: message.from,
-            Text: message.text,
-            createdAt: new Date().getTime()
-        });
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('this is from the server.');
     });
 
     socket.on('disconnect', () => {
